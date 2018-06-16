48f952ee0985c87236c61d43d5561a04d8394e87
Mandeep Singh <mandeep.singh@sprinklr.com>
2018-06-16 11:07:38 +0530

    minor changes1

--- src/main/java/com/example/servlet/session/LoginServlet.java
+++ src/main/java/com/example/servlet/session/LoginServlet.java
@@ -17,33 +17,33 @@ import javax.servlet.http.HttpSession;
  */
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	private final String userID = "admin";
-	private final String password = "password";
+    private static final long serialVersionUID = 1L;
+    private final String userID = "admin";
+    private final String password = "password";
 
-	protected void doPost(HttpServletRequest request,
-			HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request,
+                          HttpServletResponse response) throws ServletException, IOException {
 
-		// get request parameters for userID and password
-		String user = request.getParameter("user");
-		String pwd = request.getParameter("pwd");
+        // get request parameters for userID and password
+        String user = request.getParameter("user");
+        String pwd = request.getParameter("pwd");
 
-		if(userID.equals(user) && password.equals(pwd)){
-			HttpSession session = request.getSession();
-			session.setAttribute("user", "Pankaj");
-			//setting session to expiry in 30 mins
-			session.setMaxInactiveInterval(30*60);
-			Cookie userName = new Cookie("user", user);
-			userName.setMaxAge(30*60);
-			response.addCookie(userName);
-			response.sendRedirect("LoginSuccess.jsp");
-		}else{
-			RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
-			PrintWriter out= response.getWriter();
-			out.println("<font color=red>Either user name or password is wrong.</font>");
-			rd.include(request, response);
-		}
+        if (userID.equals(user) && password.equals(pwd)) {
+            HttpSession session = request.getSession();
+            session.setAttribute("user", "Pankaj");
+            //setting session to expiry in 30 mins
+            session.setMaxInactiveInterval(30 * 60);
+            Cookie userName = new Cookie("user", user);
+            userName.setMaxAge(30 * 60);
+            response.addCookie(userName);
+            response.sendRedirect("LoginSuccess.jsp");
+        } else {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out = response.getWriter();
+            out.println("<font color=red>Either user name or password is wrong.</font>");
+            rd.include(request, response);
+        }
 
-	}
+    }
 
 }
--- src/main/java/com/example/servlet/session/LogoutServlet.java
+++ src/main/java/com/example/servlet/session/LogoutServlet.java
@@ -15,27 +15,27 @@ import javax.servlet.http.HttpSession;
  */
 @WebServlet("/LogoutServlet")
 public class LogoutServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	response.setContentType("text/html");
-    	Cookie[] cookies = request.getCookies();
-    	if(cookies != null){
-    	for(Cookie cookie : cookies){
-    		if(cookie.getName().equals("JSESSIONID")){
-    			System.out.println("JSESSIONID="+cookie.getValue());
-    			break;
-    		}
-    	}
-    	}
-    	//invalidate the session if exists
-    	HttpSession session = request.getSession(false);
-		// todo session might me null{ in case of request has no valid HttpSession, this method returns NULL}
-    	System.out.println("User="+session.getAttribute("user"));
-    	if(session != null){
-    		session.invalidate();
-    	}
-    	response.sendRedirect("login.html");
+        response.setContentType("text/html");
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (cookie.getName().equals("JSESSIONID")) {
+                    System.out.println("JSESSIONID=" + cookie.getValue());
+                    break;
+                }
+            }
+        }
+        //invalidate the session if exists
+        HttpSession session = request.getSession(false);
+        // todo session might me null{ in case of request has no valid HttpSession, this method returns NULL}
+        System.out.println("User=" + session.getAttribute("user"));
+        if (session != null) {
+            session.invalidate();
+        }
+        response.sendRedirect("login.html");
     }
 
 }
